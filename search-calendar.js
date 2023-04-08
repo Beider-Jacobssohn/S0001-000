b872ef3ca6015214a8e2099808a26706d581cd3b
Beider-Jacobssohn <w5ghkqfi@duck.com>
2023-04-08 18:27:37 +0300

    Some MJS to JS files were previously uncaught.

--- search-calendar.js
+++ search-calendar.js
@@ -1,32 +1,29 @@
-import { google } from 'googleapis';
+const { google } = require('googleapis');
 
-const searchCalendar = async (auth, searchTerms) => {
+const searchCalendar = async (auth, searchTerms, startDate, endDate) => {
     const calendar = google.calendar({ version: 'v3', auth });
-    const now = new Date();
-    const last30Days = new Date(now.getTime() - 30 * 24 * 60 * 60 * 1000);
 
     const events = await calendar.events.list({
         calendarId: 'primary',
-        timeMin: last30Days.toISOString(),
-        timeMax: now.toISOString(),
+        timeMin: startDate.toISOString(),
+        timeMax: endDate.toISOString(),
         singleEvents: true,
         orderBy: 'startTime',
         maxResults: 1000,
         fields: 'items(id,summary,start,end)',
     });
 
+
+
     // Search for events that match the search terms
-    const matchingEvents = events.data.items.filter(event => {
+    // Return matching events
+    return events.data.items.filter(event => {
         return searchTerms.some(term => {
             return (
                 event.summary.toLowerCase().includes(term.toLowerCase()) ||
                 (event.description && event.description.toLowerCase().includes(term.toLowerCase()))
             );
         });
-    });
-
-    // Return the matching events
-    return matchingEvents;
+    })
 }
-
-export default searchCalendar;
+module.exports = searchCalendar;
